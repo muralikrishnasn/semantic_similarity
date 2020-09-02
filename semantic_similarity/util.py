0a1561feb6dab1f7f0ed018487ecd406297f9e52
Emil Nuutinen <emil.nuutinen@gmail.com>
2020-09-02 16:48:13 +0300

    create util file with vector functions for all the models

--- semantic_similarity/util.py
+++ semantic_similarity/util.py
@@ -1,15 +1,11 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
-
 import fasttext
 import fasttext.util
-
-from laserembeddings import Laser
-
-from sentence_transformers import SentenceTransformer
-
 import tensorflow as tf
 import tensorflow_hub as hub
 
+from laserembeddings import Laser
+from sentence_transformers import SentenceTransformer
 from scipy.spatial import distance
 
 
@@ -20,7 +16,9 @@ def tfidf(sentences):
     return sentence_vectors
 
 
-def fasttext(sentences):
+def fasttext_vec(sentences):
+    fasttext.util.download_model('en', if_exists='ignore')  # English
+    ft = fasttext.load_model('cc.en.300.bin')
     sentence_vectors = [ft.get_sentence_vector(x) for x in sentences]
 
     return sentence_vectors
@@ -36,6 +34,7 @@ def laser(sentences):
 def sentence_bert(sentences):
     model = SentenceTransformer('bert-large-nli-stsb-mean-tokens')
     sentence_embeddings = model.encode(sentences)
+
     return sentence_embeddings
 
 
@@ -43,8 +42,9 @@ def use(sentences):
     module_url = "https://tfhub.dev/google/universal-sentence-encoder/4"
     model = hub.load(module_url)
     sentence_embeddings = model(sentences)
+
     return sentence_embeddings
 
 
-def cosine_similarity(first, second):
+def cosine_distance(first, second):
     return distance.cosine(first, second)
