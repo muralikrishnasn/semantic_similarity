b86684e61bfdc3b1b3cd391300b376532580143e
Emil Nuutinen <emil.nuutinen@gmail.com>
2020-09-09 18:58:30 +0300

    change plot scale back to 0..1 and change correlation to cosine similarity

--- semantic_similarity/test_similarities/utils.py
+++ semantic_similarity/test_similarities/utils.py
@@ -1,22 +1,24 @@
 import seaborn as sns
-import numpy as np
 
 from scipy.spatial import distance
+from sklearn.metrics.pairwise import cosine_similarity
 
 
-def cosine_similarity(first, second):
-    similarity = 1 - distance.cosine(first, second)
+def similarity(first, second):
+    sim = 1 - distance.cosine(first, second)
 
-    return similarity
+    return sim
 
 
 def plot_similarity(labels_y, labels_x, features_y, features_x):
-    corr = np.inner(features_y, features_x)
+    corr = cosine_similarity(features_y, features_x)
     sns.set(font_scale=1.2)
     g = sns.heatmap(
         corr,
         xticklabels=labels_x,
         yticklabels=labels_y,
+        vmin=0,
+        vmax=1,
         cmap="YlOrRd")
     g.set_xticklabels(labels_x, rotation=90)
     g.set_yticklabels(labels_y, rotation=0)
