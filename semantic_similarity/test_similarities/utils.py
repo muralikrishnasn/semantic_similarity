bc2e46638680bc5196832db6dcc3825d77d29439
Emil Nuutinen <emil.nuutinen@gmail.com>
2020-09-10 13:06:52 +0300

    add printing function and change function names

--- semantic_similarity/test_similarities/utils.py
+++ semantic_similarity/test_similarities/utils.py
@@ -4,7 +4,7 @@ from scipy.spatial import distance
 from sklearn.metrics.pairwise import cosine_similarity
 
 
-def similarity(first, second):
+def get_similarity(first, second):
     sim = 1 - distance.cosine(first, second)
 
     return sim
@@ -22,3 +22,9 @@ def plot_similarity(labels_y, labels_x, features_y, features_x):
         cmap="YlOrRd")
     g.set_xticklabels(labels_x, rotation=90)
     g.set_yticklabels(labels_y, rotation=0)
+
+
+def print_sentences(categories, sentences):
+    for (category, sentence) in zip(categories, sentences):
+        print(f'{category}: {sentence}')
+        print()
